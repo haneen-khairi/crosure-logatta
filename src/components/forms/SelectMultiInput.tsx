0ceede5d5f24526f431e5b030e4ad00173130259
John1316 <johnmaher179@gmail.com>
2023-11-16 21:40:31 +0200

    check responsive

--- src/components/forms/SelectMultiInput.tsx
+++ src/components/forms/SelectMultiInput.tsx
@@ -1,25 +1,7 @@
 import { Checkbox, Input, Popover, PopoverBody, PopoverContent, PopoverTrigger } from "@chakra-ui/react";
 
 import { computedInputProps } from "./FormRenderer";
-// import { useState } from "react";
-import Select from "react-select";
-// import "./styles.css";
-const customStyles = {
-    menu: (provided: any, state: any) => ({
-      ...provided,
-      width: "100%",
-    //   borderBottom: '1px dotted pink',
-      color: state.selectProps.menuColor,
-      padding: 10,
-      zIndex: 999
-    }),
-    option: (provided: any) => ({
-      ...provided,
-      fontSize: '12px', // Adjust this value to change the font size
-      cursor: 'pointer'
-      // other styles you want to apply to the options
-    }),
-}
+
 
 const SelectMultiInput = ({ onChange, ...props }: computedInputProps) => {
   return (
@@ -67,40 +49,6 @@ const SelectMultiInput = ({ onChange, ...props }: computedInputProps) => {
         </PopoverBody>
       </PopoverContent>
     </Popover>
-  //   <div className="select__menu">
-  //   <Select
-  //   // id={id}
-  //   //   defaultValue={allOptions[defaultValue]}
-  //     isMulti
-  //     styles={customStyles}
-  //     className="select__menu--data"
-  //     // // closeMenuOnSelect={false}
-  //     // // hideSelectedOptions={false}
-  //     onChange={(option: any) => {
-  //       // onChange(
-  //       //   props.name,
-  //       //   props.value?.includes(option.value)
-  //       //     ? props.value?.filter(
-  //       //         (v: string | boolean | number) => v !== option.value
-  //       //       )
-  //       //     : [...props.value, option.value]
-  //       // )
-  //       console.log('=== options ===', option , props)
-  //       console.log('=== props name ===' , props.name);
-  //       console.log('=== props value ===' , props.value?.includes(option.value)
-  //                   ? props.value?.filter(
-  //                       (v: string | boolean | number) => v !== option.value
-  //                     )
-  //                   : [...props.value, option.value]
-  //               )
-  //       // getDataBack(option)
-  //     }}
-  //     options={props.options}
-  //     // components={{
-  //     //   Option: SelectOptions
-  //     // }}
-  //   />
-  // </div>
   );
 };
 
