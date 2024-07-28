c6093243c176c8a6cf2210fa8da69e7e1d356aaf
John1316 <johnmaher179@gmail.com>
2024-07-29 01:31:34 +0300

    add chart box

--- src/views/app/resultsPage/ChartsBox.tsx
+++ src/views/app/resultsPage/ChartsBox.tsx
@@ -1,4 +1,4 @@
-import { GridItem, SimpleGrid, Text } from "@chakra-ui/react";
+import { Button, GridItem, SimpleGrid, Text } from "@chakra-ui/react";
 import { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
@@ -9,8 +9,8 @@ import BoxTitle from "../../../components/typography/BoxTitle";
 import AreaChart from "./AreaChart";
 // import SelectComponent from "../../../components/SelectComponent";
 import InputTypePicker from "../../../components/forms/InputTypePicker";
-import TableComponent from "../../../components/TableComponent";
-import axios from "axios";
+import * as XLSX from "xlsx"
+
 
 const ResultsChartsBox = () => {
   const [stats, setStats] = useState({ fires: [
@@ -26,6 +26,9 @@ const ResultsChartsBox = () => {
       }
     
   ] });
+  const [crimesStats, setCrimesStats] = useState<any[]>([])
+  const [firesStats, setFiresStats] = useState<any[]>([])
+
   // async function getFireIncidents(){
   //   try {
   //     const response = await StatsAPI.fireIncidents()
@@ -73,7 +76,58 @@ const ResultsChartsBox = () => {
   const { postcode } = useSelector(
     (_: { data: { postcode: string } }) => _.data
   );
-
+  function sumAllYears(dataArray: any[]) {
+    return dataArray.reduce((total: any, item: any) => {
+      return total + item.stats.reduce((yearTotal: any, yearData: any) => {
+        return yearTotal + yearData.stats;
+      }, 0);
+    }, 0);
+  }
+  function calculateYearStats(dataArray: any, key: string) {
+    const yearStats = {};
+  
+    dataArray.forEach((item: any) => {
+      item.stats.forEach((yearData: any) => {
+        const year = yearData.year;
+        const stats = yearData[key];
+        yearStats[year] = (yearStats[year] || 0) + stats;
+      });
+    });
+  
+    return yearStats;
+  }
+  function sumAllYearsFires(dataArray: any[]) {
+    return dataArray.reduce((total: any, item: any) => {
+      return total + item.stats.reduce((yearTotal: any, yearData: any) => {
+        return yearTotal + yearData.incidents_count;
+      }, 0);
+    }, 0);
+  }
+  function convertToArrayOfObjects(yearStatsObject: any) {
+    return Object.entries(yearStatsObject).map(([year, totalStats]) => ({ year: Number(year), totalStats }));
+  }
+  function exportToExcel(dataArray: any, name: any) {
+    // Create a new workbook and worksheet
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.json_to_sheet(dataArray);
+  
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Data');
+  
+    // Generate the Excel file
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+  
+    // Create a blob and download the file
+    const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const url = window.URL.createObjectURL(blob);   
+  
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `${name}.xlsx`;
+    link.click();
+    window.URL.revokeObjectURL(url);   
+  
+  }
   useEffect(() => {
     StatsAPI.crimes({
       postcode,
@@ -83,6 +137,12 @@ const ResultsChartsBox = () => {
     }).then((crimes: any) => {
       // @ts-ignore
       console.log('=== crimes ===',crimes)
+      const sumOfCrimes = sumAllYears(crimes)
+      const sumOfCrimesTable = calculateYearStats(crimes, 'stats')
+      const sumOfCrimesTableArray = convertToArrayOfObjects(sumOfCrimesTable)
+      setCrimesStats(sumOfCrimesTableArray)
+
+      console.log("🚀 ~ useEffect ~ sumOfCrimes:", sumOfCrimes , sumOfCrimesTableArray)
       console.log('=== crimes includes ===', chartSelectedOptions.crimes.includes('type'))
       if(crimes[0].type === undefined){
         setStats((current) => ({ 
@@ -107,7 +167,11 @@ const ResultsChartsBox = () => {
     }).then((fires: any) => {
       // @ts-ignore
       console.log('=== fires ===', fires)
-
+      const sumOfFires = sumAllYearsFires(fires)
+      const sumOfFiresTable = calculateYearStats(fires, 'incidents_count')
+      const sumOfFiresTableArray = convertToArrayOfObjects(sumOfFiresTable)
+      setFiresStats(sumOfFiresTableArray)
+      console.log("🚀 ~ useEffect ~ sumOffires:", sumOfFires, sumOfFiresTableArray)
       if(fires[0].type === undefined){
         setStats((current) => ({ 
           ...current, 
@@ -126,6 +190,7 @@ const ResultsChartsBox = () => {
 
     });
     console.log('=== stats ===', stats)
+    
   }, [postcode, chartSelectedOptions]);
   const ChartCard = ({
     title = "",
@@ -164,6 +229,26 @@ const ResultsChartsBox = () => {
                 ]}
                 tableName={"fire_incidents"}
               /> */}
+              <Button
+              w="fit-content"
+              colorScheme="primary"
+              type="submit"
+              color={'#fff'}
+              borderRadius={'50px'}
+              fontSize={'14px'}
+              // style={{
+              //   // width: 'fit-content',
+              //   margin: '0 auto'
+              // }}
+              // borderRadius={borderRound}
+              py="7"
+              onClick={() => name === "crimes" ?
+                exportToExcel(crimesStats, title) : exportToExcel(firesStats, title)
+              }
+            >
+              Download table 
+              {/* {tableName} in Excel */}
+            </Button>
         <GridItem className="grid-column-select">
           {/* <SelectComponent
           id={`data_crimes_${name}`}
